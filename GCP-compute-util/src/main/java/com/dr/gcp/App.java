bae83898c838ad346d38917e58f4cd7fe9c73a5f
dr-ruminski <dr.ruminski@gmail.com>
2020-07-25 13:36:30 +0200

    Added demo of VM manager.

--- GCP-compute-util/src/main/java/com/dr/gcp/App.java
+++ GCP-compute-util/src/main/java/com/dr/gcp/App.java
@@ -1,9 +1,33 @@
 package com.dr.gcp;
 
+import java.util.Scanner;
+
+import com.dr.gcp.compute.vm.manager.VMManager;
+
 /**
- *
+ * Test app that demonstrates a small susbset of the VM manager functionality.
  */
 public class App {
 	public static void main(String[] args) {
+
+		// initialize a VM Manager instance
+		VMManager vmManager = new VMManager();
+		
+		// starts randomly selected GCE instance
+		vmManager.startRandomInstance();
+		
+		// gets the IP address of randomly selected VM instance that runs on GCP
+		String ip = vmManager.getCurrentVM().getNetworkInterfaces().get(0).getAccessConfigs().get(0).getNatIP();
+		
+		System.out.println(ip);
+		
+		// hit enter to shutdown randomly selected a VM instance
+		Scanner s = new Scanner(System.in);
+		s.nextLine();
+		s.close();
+		
+		// stops VM instance
+		vmManager.stopInstance(vmManager.getCurrentVM().getName());
+		
 	}
 }
