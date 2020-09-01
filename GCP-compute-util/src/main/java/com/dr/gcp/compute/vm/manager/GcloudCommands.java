397a17805e92750afb29f6c1b38a9fc9e515e26b
dr-ruminski <dr.ruminski@gmail.com>
2020-09-01 15:13:39 +0200

    Added a private constructor in order to prevent class instantiation.

--- GCP-compute-util/src/main/java/com/dr/gcp/compute/vm/manager/GcloudCommands.java
+++ GCP-compute-util/src/main/java/com/dr/gcp/compute/vm/manager/GcloudCommands.java
@@ -6,4 +6,7 @@ public class GcloudCommands {
 	public static final String LIST_INSTANCES = GCLOUD_PATH+"gcloud compute instances list --format json";
 	public static final String START_INSTANCE = GCLOUD_PATH+"gcloud compute instances start ";
 	public static final String STOP_INSTANCE = GCLOUD_PATH+"gcloud compute instances stop ";
+	
+	// Prevents instantiation
+	private GcloudCommands() {}
 }
