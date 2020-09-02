bfe81c92df24e8b63774ac3ef700d17201cc30f0
dr-ruminski <dr.ruminski@gmail.com>
2020-09-02 10:15:58 +0200

    Redesigned Manager class in order to extend it to handle generic data model (in future to process Azure, Amazon, Alibaba models etc.). Fixed bug related to memory leak.

--- GCP-compute-util/src/main/java/com/dr/gcp/compute/vm/manager/Manager.java
+++ GCP-compute-util/src/main/java/com/dr/gcp/compute/vm/manager/Manager.java
@@ -3,44 +3,52 @@ package com.dr.gcp.compute.vm.manager;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.Logger;
 
-public abstract class Manager {
+import com.dr.gcp.compute.vm.model.GCPComputeVModel;
 
-	protected String execute(Logger log, String command) {
-		try {
-			Process process = Runtime.getRuntime().exec(command);
+public abstract class Manager<T> implements IVMManager {
 
-			// reads I/O of the gcloud tool
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+	// maps vm name to its model
+	protected Map<String, List<T>> vmsMap = new HashMap<String, List<T>>();
+	
+	// current virtual machine that is managed by a VMManager instance
+	protected GCPComputeVModel currVM = null;
 
-			// read the output from the command
-			String in = null;
-			StringBuilder input = new StringBuilder();
-			while ((in = stdInput.readLine()) != null) {
-				input.append(in).append('\n');
-			}
-			if (input.length() > 0)
-				log.trace(input);
-			
-			StringBuilder errorInput = new StringBuilder();
-			// read any errors from the attempted command
-			while ((in = stdError.readLine()) != null) {
-				errorInput.append(in).append('\n');
-			}
+	// 
+	protected String execute(Logger log, String command) throws IOException {
+		Process process = Runtime.getRuntime().exec(command);
+		// reads I/O of the gcloud tool
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
-			if (errorInput.length() > 0) {
-				log.error(errorInput.toString());
-			}
-			
-			return input.toString();
-
-		} catch (IOException e) {
-			log.error(e);
+		// read the output from the command
+		String in = null;
+		StringBuilder input = new StringBuilder();
+		while ((in = stdInput.readLine()) != null) {
+			input.append(in).append('\n');
 		}
+		if (input.length() > 0)
+			log.trace(input);
+
+		StringBuilder errorInput = new StringBuilder();
+		// read any errors from the attempted command
+		while ((in = stdError.readLine()) != null) {
+			errorInput.append(in).append('\n');
+		}
+
+		if (errorInput.length() > 0)
+			log.error(errorInput.toString());
+
+		// cleaning resources and killing the process 
+		stdInput.close();
+		stdError.close();
+		process.destroy();
 		
-		return null;
+		return input.toString();
 	}
 }
