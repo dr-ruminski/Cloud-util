3c611dc5145d5147e82d39aff2830126c6fba3e8
dr-ruminski <dr.ruminski@gmail.com>
2020-09-02 10:12:55 +0200

    A small change in docs.

--- GCP-compute-util/src/main/java/com/dr/gcp/compute/vm/manager/IVMManager.java
+++ GCP-compute-util/src/main/java/com/dr/gcp/compute/vm/manager/IVMManager.java
@@ -1,9 +1,11 @@
 package com.dr.gcp.compute.vm.manager;
 
+import java.io.IOException;
+
 /**
  * An interface for the Virtual Machine Manager. The idea of use
  * {@link IVMManager} is to support e.g. Amazon, Alibaba, Azure clouds managers.
- * Currently is used by GCP {@link VMManager}.
+ * Currently is used by {@link VMManager} in context of Google Cloud.
  * 
  * @author Dariusz Rumiński
  *
@@ -15,33 +17,33 @@ public interface IVMManager {
 	 * 
 	 * @param name - Google Cloud Engine instance name
 	 */
-	void startInstance(String name);
+	void startInstance(String name) throws IOException;
 
 	/**
 	 * Stops virtual machine.
 	 * 
 	 * @param name - Google Cloud Engine instance name
 	 */
-	void stopInstance(String name);
+	void stopInstance(String name) throws IOException;
 
 	/**
 	 * Starts all GCE instances one by one.
 	 */
-	void startAllInstance();
+	void startAllInstance() throws IOException;
 
 	/**
 	 * Stops all GCE instances one by one.
 	 */
-	void stopAllInstance();
+	void stopAllInstance() throws IOException;
 
 	/**
 	 * Selects randomly a GCE instance and starts it.
 	 */
-	void startRandomInstance();
+	void startRandomInstance() throws IOException;
 
 	/**
 	 * Stops randomly selected GCE instance.
 	 */
-	void stopRandomInstance();
+	void stopRandomInstance() throws IOException;
 
 }
